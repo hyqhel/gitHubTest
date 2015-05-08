d70d5ea7dd972b098bf8e51eb79570f935df9286
hyqhel <huangyq3@asiainfo.com>
2015-05-08 16:15:20 +0800

    adfa

---
+++ maven_web/src/main/java/com/juvenxu/mvnbook/helloworld/HelloWorld.java
@@ -0,0 +1,9 @@
+package com.juvenxu.mvnbook.helloworld;
+public class HelloWorld {
+  public String sayHello(){
+	  return	"hello maven";
+  }
+  public static void main(String[] args) {
+	System.out.println(new HelloWorld().sayHello());
+}
+}
---
+++ maven_web/src/test/java/com/juvenxu/mvnbook/helloworld/HelloWorldTest.java
@@ -0,0 +1,14 @@
+package com.juvenxu.mvnbook.helloworld;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class HelloWorldTest {
+	@Test
+   public void testSayHello(){
+	   HelloWorld helloWorld =new HelloWorld();
+	   String result=helloWorld.sayHello();
+	   assertEquals("hello maven",result);
+   }
+}
