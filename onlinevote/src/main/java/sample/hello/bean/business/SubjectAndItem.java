cc62541632de970c26c6bd46607dca2a81de8886
dongwh <dongwh@dongwh.ai.com>
2015-05-15 18:07:06 +0800

    add business bean SubjectAndItem

---
+++ onlinevote/src/main/java/sample/hello/bean/business/SubjectAndItem.java
@@ -0,0 +1,35 @@
+package sample.hello.bean.business;
+
+public class SubjectAndItem {
+
+	private String subjectId;
+	private String subjectContent;
+	private String itemId;
+	private String itemContent;
+	
+	public String getSubjectId() {
+		return subjectId;
+	}
+	public void setSubjectId(String subjectId) {
+		this.subjectId = subjectId;
+	}
+	public String getSubjectContent() {
+		return subjectContent;
+	}
+	public void setSubjectContent(String subjectContent) {
+		this.subjectContent = subjectContent;
+	}
+	public String getItemId() {
+		return itemId;
+	}
+	public void setItemId(String itemId) {
+		this.itemId = itemId;
+	}
+	public String getItemContent() {
+		return itemContent;
+	}
+	public void setItemContent(String itemContent) {
+		this.itemContent = itemContent;
+	}
+	
+}
