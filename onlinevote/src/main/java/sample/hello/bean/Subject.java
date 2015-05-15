4b9e134c052c0c7caa4a014bc4339057dfffed35
dongwh <dongwh@dongwh.ai.com>
2015-05-15 17:20:06 +0800

    add subject bean

---
+++ onlinevote/src/main/java/sample/hello/bean/Subject.java
@@ -0,0 +1,56 @@
+package sample.hello.bean;
+
+public class Subject {
+
+	private String subjectId;
+	private String subjectContent;
+	private String subjectStatus;
+	private String startTime;
+	private String endTime;
+	private String type;
+	private String maxSelect;
+	
+	public String getSubjectId() {
+		return subjectId;
+	}
+	public void setSubjectId(String subjectId) {
+		this.subjectId = subjectId;
+	}
+	public String getSubjectContent() {
+		return subjectContent;
+	}
+	public void setSubjectContent(String subjectContent) {
+		this.subjectContent = subjectContent;
+	}
+	public String getSubjectStatus() {
+		return subjectStatus;
+	}
+	public void setSubjectStatus(String subjectStatus) {
+		this.subjectStatus = subjectStatus;
+	}
+	public String getStartTime() {
+		return startTime;
+	}
+	public void setStartTime(String startTime) {
+		this.startTime = startTime;
+	}
+	public String getEndTime() {
+		return endTime;
+	}
+	public void setEndTime(String endTime) {
+		this.endTime = endTime;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getMaxSelect() {
+		return maxSelect;
+	}
+	public void setMaxSelect(String maxSelect) {
+		this.maxSelect = maxSelect;
+	}
+	
+}
