f413b008211d67400b17257e4984291aa7bae64e
hyqhel <huangyq3@asiainfo.com>
2015-05-25 16:57:58 +0800

    a

---
+++ Baas/src/main/java/com/asiainfo/baas/bean/User.java
@@ -0,0 +1,13 @@
+package com.asiainfo.baas.bean;
+public class User {
+
+	private String userName;
+
+	public String getUserName() {
+		return this.userName;
+	}
+
+	public void setUserName(String value) {
+		this.userName = value;
+	}
+}
