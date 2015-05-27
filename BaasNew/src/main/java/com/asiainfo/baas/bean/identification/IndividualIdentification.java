de8de406944e4784e286bece5744b0a60e6ed6c6
dongwh <dongwh@dongwh.ai.com>
2015-05-27 18:31:51 +0800

    add individualIdentification bean

--- BaasNew/src/main/java/com/asiainfo/baas/bean/identification/IndividualIdentification.java
+++ BaasNew/src/main/java/com/asiainfo/baas/bean/identification/IndividualIdentification.java
@@ -10,4 +10,22 @@ public abstract class IndividualIdentification extends PartyIdentification {
 	public Individual _individual;
 	
 	public String IndividualType;
+
+	public Individual get_individual() {
+		return _individual;
+	}
+
+	public void set_individual(Individual _individual) {
+		this._individual = _individual;
+	}
+
+	public String getIndividualType() {
+		return IndividualType;
+	}
+
+	public void setIndividualType(String individualType) {
+		IndividualType = individualType;
+	}
+	
+	
 }
