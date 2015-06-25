8e606355df13cda7e70a41dbab87bef23cdc92a9
dongwh <dongwh@dongwh.ai.com>
2015-06-25 14:39:46 +0800

    add Class of constant about ProductSpec

---
+++ BaaSFive/src/main/java/com/ai/baas/common/constant/Const.java
@@ -0,0 +1,13 @@
+package com.ai.baas.common.constant;
+
+public class Const {
+
+	
+	/**
+	 * ProductSpecification常量定义
+	 */
+	
+	/*ProdSpecChar常量定义*/
+	public static final String SPEC_CHAR_VALUE_TYPE_TEXT = "1";
+	public static final String SPEC_CHAR_VALUE_TYPE_NUMERIC = "2";
+}
