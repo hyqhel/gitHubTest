6938fbd97b9f3d7544770416f69eea8c17b26b38
dongwh <dongwh@dongwh.ai.com>
2015-06-26 13:15:17 +0800

    update create prodSpec

--- BaaSFive/src/test/java/com/ai/baas/product/spec/ProductSpecificationTest.java
+++ BaaSFive/src/test/java/com/ai/baas/product/spec/ProductSpecificationTest.java
@@ -4,6 +4,7 @@ import java.text.ParseException;
 import java.util.List;
 
 import org.junit.After;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import com.ai.baas.basetype.TimePeriod;
@@ -11,15 +12,18 @@ import com.ai.baas.common.constant.Const;
 import com.ai.baas.common.util.DateUtils;
 
 public class ProductSpecificationTest {
-	String[] specSelectCharIds = {"1","2","4"};
+	
+	String[] specSelectCharIds = {"1","2","4","3"};
+	String[] specSelectCharValueIds = {"11","12","14","13"};
 	int[][] specCharUseValueRelate = {
 			{0,0},
-			{0,1},
-			{2,2}
+			{2,2},
+			{3,3}
 	};
-	String[] specSelectCharValueIds = {"11","12","15"};
 	
 	private ProductSpecification atomicProdSpec;
+	
+	private static TimePeriod validFor;
 	
 	public ProductSpecification getAtomicProdSpec() {
 		return atomicProdSpec;
@@ -35,48 +39,29 @@ public class ProductSpecificationTest {
 	 */
 	@Test
 	public void createProdSpec() throws ParseException{
-		String productNumber = "1";
-		String name = "11 英寸 MacBook Air";
-		String brand = "apple";
-		String description = "Mac";
-		TimePeriod validFor = new TimePeriod();
-		String startDate = "2015-06-01";
-		String endDate = "2015-08-01";
-		validFor.setStartDateTime(DateUtils.str2Date(startDate, DateUtils.date_sdf));
-		validFor.setEndDateTime(DateUtils.str2Date(endDate, DateUtils.date_sdf));
 		
 		/*new AtomicProductSpecification Object*/
-		atomicProdSpec = new AtomicProductSpecification(productNumber, name, brand, description, validFor);
+		atomicProdSpec = new AtomicProductSpecification("1", "11 英寸 MacBook Air", "apple", "Mac", validFor);
 		
 		ProductSpecCharacteristicTest prodSpecCharTest = new ProductSpecCharacteristicTest();
 		/*get list ProductSpecCharacteristic Object */
 		List<ProductSpecCharacteristic> specCharList = prodSpecCharTest.getProdSpecChars();
 		
-		boolean isDefault = false;
-		boolean canBeOveridden = false;
-		boolean isPackage = true;
-		String nameUse ="char_Use";
-		String unique = ""; 
-		int minCardinality = 1;
-		int maxCardinality =3;
-		boolean extensible = false;
-		String descriptionUse = "this is a description about size and weight used by CharUse";
-		
 		for (int i = 0; i < specSelectCharIds.length; i++) {
-			//selectedProdChar(specCharList, specCharIds[i], specCharValueIds);
 			/*new CharUse Object*/
 			ProductSpecCharacteristic selectedSpecChar = selectedProdChar(specCharList,specSelectCharIds[i]);
-			atomicProdSpec.addCharacteristic(selectedSpecChar, canBeOveridden, isPackage, validFor, nameUse,  unique, minCardinality, maxCardinality, extensible,  descriptionUse);
+			atomicProdSpec.addCharacteristic(selectedSpecChar, false, true, validFor, selectedSpecChar.getName(),  "", 1, 3, false,  "this is a description about size and weight used by CharUse");
 			List<ProductSpecCharacteristicValue> prodSpecCharValueList = selectedSpecChar.getProdSpecCharValue();
 			for(int j=0;j<specCharUseValueRelate.length;j++){
 				if(i==specCharUseValueRelate[j][0]){
 					int valuesub=specCharUseValueRelate[j][1];
 					ProductSpecCharacteristicValue selectvalue=selectedProdCharValue(prodSpecCharValueList,specSelectCharValueIds[valuesub]);
-					atomicProdSpec.attachCharacteristicValue(selectedSpecChar, selectvalue, isDefault, validFor);
+					atomicProdSpec.attachCharacteristicValue(selectedSpecChar, selectvalue, false, validFor);
 				}
 			}
 		}
 	}
+	
 	private ProductSpecCharacteristic selectedProdChar(List<ProductSpecCharacteristic> specCharList, String selectedProdSpecCharId){
 		String prodSpecCharId = "";
 		ProductSpecCharacteristic prodSpecSelectChar=null;
@@ -108,12 +93,12 @@ public class ProductSpecificationTest {
 	@After
 	public void  printResult(){
 		/*print result*/
-		System.out.println(atomicProdSpec.getName());
+		System.out.println("规格："+atomicProdSpec.getName());
 		List<ProductSpecCharUse> prodSpecCharUseList = atomicProdSpec.getProdSpecChar();
 		if(null!=prodSpecCharUseList && prodSpecCharUseList.size()>0){
 			for (int i = 0; i < prodSpecCharUseList.size(); i++) {
 				ProductSpecCharUse  prodSpecCharUse = prodSpecCharUseList.get(i);
-				System.out.println(prodSpecCharUse.getName());
+				System.out.println("    特征："+prodSpecCharUse.getName());
 				List<ProdSpecCharValueUse> prodSpecCharValueUseList = prodSpecCharUse.getProdSpecCharValue();
 				if(null!=prodSpecCharValueUseList && prodSpecCharValueUseList.size()>0){
 					for (int j = 0; j < prodSpecCharValueUseList.size(); j++) {
@@ -122,9 +107,13 @@ public class ProductSpecificationTest {
 							if(null!=prodSpecCharValueUse.getProdSpecCharValue()){
 								//如果是from to的则输出范围值
 								if(prodSpecCharValueUse.getProdSpecCharValue().getValueType().equals(Const.SPEC_CHAR_VALUE_TYPE_FORTH)){
-									System.out.println("特征的取值范围是："+prodSpecCharValueUse.getProdSpecCharValue().getValueFrom()+"~"+prodSpecCharValueUse.getProdSpecCharValue().getValueTo());
+									//System.out.println("特征的取值范围是："+prodSpecCharValueUse.getProdSpecCharValue().getValueFrom()+"~"+prodSpecCharValueUse.getProdSpecCharValue().getValueTo());
+									String validFrom = prodSpecCharValueUse.getProdSpecCharValue().getValueFrom();
+									String rangeInterval = prodSpecCharValueUse.getProdSpecCharValue().getRangeInterval();
+									String theValue = String.valueOf(Double.parseDouble(validFrom)+Double.parseDouble(rangeInterval));
+									System.out.println("         特征值："+theValue+prodSpecCharValueUse.getProdSpecCharValue().getUnitOfMeasure());
 								}else{
-									System.out.println("特征值是："+prodSpecCharValueUse.getProdSpecCharValue().getValue());
+									System.out.println("         特征值："+prodSpecCharValueUse.getProdSpecCharValue().getValue()+prodSpecCharValueUse.getProdSpecCharValue().getUnitOfMeasure());
 								}
 							}
 							
@@ -133,5 +122,18 @@ public class ProductSpecificationTest {
 				}
 			}
 		}
+	}
+	
+	@BeforeClass
+	public static void initVliadFor(){
+		validFor = new TimePeriod();
+		String startDate = "2015-06-01";
+		String endDate = "2015-08-01";
+		validFor.setStartDateTime(DateUtils.str2Date(startDate, DateUtils.date_sdf));
+		validFor.setEndDateTime(DateUtils.str2Date(endDate, DateUtils.date_sdf));
+	}
+	
+	public void getAllRootProdSpec(){
+		
 	}
 }
