573a76a4647e9d647eb12153594098aac7cad727
liuwei29 <liuwei29@asiainfo.com>
2015-07-08 17:08:04 +0800

    timePeriod

--- BaaSFive/src/main/java/com/ai/baas/basetype/TimePeriod.java
+++ BaaSFive/src/main/java/com/ai/baas/basetype/TimePeriod.java
@@ -1,8 +1,10 @@
 package com.ai.baas.basetype;
 
-import java.util.Date;
+import org.apache.commons.lang.StringUtils;
 
-import com.ai.baas.common.util.DateUtils;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * A base / value business entity used to represent a period of time, between two time points
@@ -11,14 +13,14 @@ public class TimePeriod {
 
     /**
      * An instant of time, starting at the TimePeriod
-     * 
+     * <p/>
      * Notes:
      * If null, then represents to the beginning of time
      */
     public Date startDateTime;
     /**
      * An instant of time, ending at the TimePeriod:
-     * 
+     * <p/>
      * Notes:
      * If null, then represents to the end of time
      */
@@ -40,36 +42,37 @@ public class TimePeriod {
         this.endDateTime = endDateTime;
     }
 
-    public int compait(Date time) throws Exception{
-    	DateUtils du = new DateUtils();
-    	if(du.dateDiff('s', du.parseCalendar(time), du.parseCalendar(startDateTime))<0){
-    		return -1;
-    	}
-    	if(du.dateDiff('s', du.parseCalendar(time), du.parseCalendar(endDateTime))>0){
-    		return 1;
-    	}
-    	if(du.dateDiff('s', du.parseCalendar(time), du.parseCalendar(startDateTime))>=0 &&
-    			du.dateDiff('s', du.parseCalendar(time), du.parseCalendar(endDateTime))<=0){
-    		return 0;
-    	}
-    	return 0;
+    public TimePeriod(String startDateTime, String endDateTime) {
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+        try {
+            if (StringUtils.isNotEmpty(startDateTime)) {
+                this.startDateTime = format.parse(startDateTime);
+            }
+            if (StringUtils.isNotEmpty(endDateTime)) {
+                this.endDateTime = format.parse(endDateTime);
+            }
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
     }
-    
+
+
     /**
      * 0  表示在区间
      * -1 小于开始时间
      * 1  大于结束时间
+     *
      * @param time
      * @return
      */
-    public int isInTimePeriod(Date time){
+    public int isInTimePeriod(Date time) {
         if (this.startDateTime != null && this.endDateTime != null) {
-            if(time.compareTo(this.startDateTime) == 1 && time.compareTo(this.endDateTime) == -1){
-            	return 0;
-            }else if(time.compareTo(this.startDateTime) == -1){
-            	return -1;
-            }else if(time.compareTo(this.endDateTime) == 1){
-            	return 1;
+            if (time.compareTo(this.startDateTime) == 1 && time.compareTo(this.endDateTime) == -1) {
+                return 0;
+            } else if (time.compareTo(this.startDateTime) == -1) {
+                return -1;
+            } else if (time.compareTo(this.endDateTime) == 1) {
+                return 1;
             }
         }
         return 0;
